a8fdc61083d6fa40d3fbf255831b1df81ce42bb0
mikajaht <mikajah.tomlinson@gmail.com>
2026-05-03 19:50:43 -0500

    HW 10 Fixes

--- Program.cs
+++ Program.cs
@@ -22,13 +22,16 @@ namespace Homework_Template
         public static string DisplayMenu()
         {
             Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("Homework 10");
             Console.WriteLine();
             Console.WriteLine("Hit [1] to run Exercise 1.");
             Console.WriteLine("Hit [2] to run Exercise 2.");
             Console.WriteLine("Hit [3] to run Exercise 3.");
             Console.WriteLine();
-            Console.WriteLine("Hit [E]: Exit");
+            Console.WriteLine("Hit [E]: Exit;");
+            Console.WriteLine();
             Console.WriteLine();
 
             var result = Console.ReadLine();
@@ -67,27 +70,35 @@ namespace Homework_Template
             // Remove "throw new NotImplementedException()" line of code 
             //  once you add your code.
 
+            // Ask the user to enter a directory path.
             string directory;
 
-            Console.WriteLine("1");
+            Console.WriteLine("Directory Information");
+            Console.WriteLine();
 
             Console.Write("Enter a directory >> ");
             directory = Console.ReadLine();
 
+            // Continue asking for directories until the user types "end".
             while (directory.ToLower() != "end")
             {
+                // Check whether the directory exists.
                 if (Directory.Exists(directory))
                 {
+                    // Store all files from the directory in an array.
                     string[] files = Directory.GetFiles(directory);
 
+                    Console.WriteLine();
                     Console.WriteLine(directory + " contains the following files");
 
+                    // Display a message if the directory has no files.
                     if (files.Length == 0)
                     {
                         Console.WriteLine("There are no files in this directory.");
                     }
                     else
                     {
+                        // Display each file in the directory.
                         foreach (string file in files)
                         {
                             Console.WriteLine("   " + file);
@@ -96,6 +107,8 @@ namespace Homework_Template
                 }
                 else
                 {
+                    // Display a message if the directory does not exist.
+                    Console.WriteLine();
                     Console.WriteLine("Directory " + directory + " does not exist");
                 }
 
@@ -113,41 +126,54 @@ namespace Homework_Template
             // Remove "throw new NotImplementedException()" line of code 
             //  once you add your code.
 
-            Console.WriteLine("2");
+            Console.WriteLine("File Comparison");
+            Console.WriteLine();
 
+            // Ask the user to enter the full path for the Word document.
             Console.Write("Enter the full path of the Word file >> ");
-            string wordFileName = Console.ReadLine();
+            string wordFileName = Console.ReadLine().Trim('"');
 
+            // Ask the user to enter the full path for the Notepad text file.
             Console.Write("Enter the full path of the Notepad file >> ");
-            string notepadFileName = Console.ReadLine();
+            string notepadFileName = Console.ReadLine().Trim('"');
 
+            Console.WriteLine();
+
+            // Make sure both files exist before comparing their sizes.
             if (File.Exists(wordFileName) && File.Exists(notepadFileName))
             {
+                // Create FileInfo objects so the program can access file details.
                 FileInfo wordFileInfo = new FileInfo(wordFileName);
                 FileInfo notepadFileInfo = new FileInfo(notepadFileName);
 
+                // Store each file size in bytes.
                 long wordFileSize = wordFileInfo.Length;
                 long notepadFileSize = notepadFileInfo.Length;
 
+                // Calculate the Notepad file size as a percentage of the Word file size.
                 double ratio = (double)notepadFileSize / wordFileSize * 100;
 
-                Console.WriteLine();
-                Console.WriteLine("The size of the Word file is " + wordFileSize);
-                Console.WriteLine("and the size of the Notepad file is " + notepadFileSize);
-                Console.WriteLine("The Notepad file is " + ratio.ToString("F2") + "% of the size of the Word file");
+                // Display the comparison results in a clean format.
+                Console.WriteLine("File Size Comparison Results");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Word file size:    " + wordFileSize + " bytes");
+                Console.WriteLine("Notepad file size: " + notepadFileSize + " bytes");
+                Console.WriteLine("Ratio: The Notepad file is " + ratio.ToString("F2") + "% of the Word file size.");
             }
             else
             {
+                // Display a helpful message if either file path is invalid.
+                Console.WriteLine("One or both files could not be found.");
                 Console.WriteLine();
 
                 if (!File.Exists(wordFileName))
                 {
-                    Console.WriteLine("The Word file does not exist.");
+                    Console.WriteLine("Word file not found: " + wordFileName);
                 }
 
                 if (!File.Exists(notepadFileName))
                 {
-                    Console.WriteLine("The Notepad file does not exist.");
+                    Console.WriteLine("Notepad file not found: " + notepadFileName);
                 }
             }
 
@@ -166,45 +192,57 @@ namespace Homework_Template
             string[] files;
             int x;
 
-            Console.WriteLine("3");
+            Console.WriteLine("Debugged Directory and File Code");
+            Console.WriteLine();
 
             Console.Write("Enter a directory: ");
             directory = Console.ReadLine();
 
+            // Check whether the directory exists before trying to get files.
             if (Directory.Exists(directory))
             {
+                // Get all files from the directory.
                 files = Directory.GetFiles(directory);
 
+                // Check whether the directory contains any files.
                 if (files.Length == 0)
                 {
                     Console.WriteLine("There are no files in " + directory);
                 }
                 else
                 {
+                    Console.WriteLine();
                     Console.WriteLine(directory + " contains the following files");
 
+                    // Display each file using a for loop.
                     for (x = 0; x < files.Length; ++x)
                     {
                         Console.WriteLine(" " + files[x]);
                     }
 
-                    Console.Write("\nEnter a file name: ");
+                    Console.WriteLine();
+                    Console.Write("Enter a file name: ");
                     fileName = Console.ReadLine();
 
+                    // Build the full file path safely.
                     path = Path.Combine(directory, fileName);
 
+                    // Check whether the selected file exists.
                     if (File.Exists(path))
                     {
+                        Console.WriteLine();
                         Console.WriteLine("File exists and was created " + File.GetCreationTime(path));
                     }
                     else
                     {
+                        Console.WriteLine();
                         Console.WriteLine(fileName + " does not exist in the " + directory + " directory");
                     }
                 }
             }
             else
             {
+                Console.WriteLine();
                 Console.WriteLine("Directory " + directory + " does not exist");
             }
 
